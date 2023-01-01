9c76e3997f45904eb4c3e2c3d11d3b54a67f59e6
caiomorozini <caiomorozini@outlook.com.br>
2023-01-01 20:03:42 -0300

    Adiciona projeto

---
+++ captura.ino
@@ -0,0 +1,20 @@
+﻿// Definindo pino do ecg em A0
+const int ECG_PIN = A0;
+
+// Declarando variavel para armazenar ECG
+int ecgData = 0;
+
+
+void setup() {
+  // Configurando baud rate para comunicação serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Lendo sinal de ecg
+  ecgData = analogRead(ECG_PIN);
+
+  // Printando saída no monitor serial
+  Serial.println(ecgData);
+
+}
